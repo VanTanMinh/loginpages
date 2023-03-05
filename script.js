51ba83f6425c548839414819ac940054a705b8bd
VanTanMinh <95200735+VanTanMinh@users.noreply.github.com>
2023-03-05 20:33:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loginForm.addEventListener('submit', (e) => {
     const email = loginForm.email.value;
     const password = loginForm.password.value;
     if (email === 'vantanminh61@gmail.com' && password === 'Tanminh2009') {
-        window.location.href = 'https://0vbry-my.sharepoint.com/personal/vantanminh_0vbry_onmicrosoft_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fvantanminh%5F0vbry%5Fonmicrosoft%5Fcom%2FDocuments%2FISO%2FWindows%2010%2Eiso&parent=%2Fpersonal%2Fvantanminh%5F0vbry%5Fonmicrosoft%5Fcom%2FDocuments%2FISO';
+        window.location.href = 'https://vantanminh.github.io/4-button-/';
     } else {
         alert('Invalid email or password');
     }
